fd6f4686fbbc59d3100b5d1755f324a3e5cbfa46
Erik <eteverson@gmail.com>
2022-07-29 20:37:33 -0700

    use event 'config-inited' to add our CSS-es

--- plasmapy_sphinx/ext/css.py
+++ plasmapy_sphinx/ext/css.py
@@ -5,12 +5,19 @@ from sphinx.application import Sphinx
 from plasmapy_sphinx.utils import css_dir
 
 
+def add_plasmapy_css(app, config):
+    path = str(css_dir)
+    if path not in config.html_static_path:
+        config.html_static_path.append(path)
+
+    app.add_css_file("admonition_color_contrast.css")
+    app.add_css_file("plasmapy.css", priority=501)
+
+
 def setup(app: Sphinx) -> None:
     app.setup_extension("sphinx_gallery.load_style")
-    app.add_css_file(
-        os.path.join(css_dir, "admonition_color_contrast.css"),
-    )
-    app.add_css_file(
-        os.path.join(css_dir, "plasmapy.css"),
-        priority=501,
-    )
+
+    # for some "unknown" reason, an extension can not add a style sheet
+    # to the sphinx build unless it's done through the 'config-inited'
+    # event
+    app.connect("config-inited", add_plasmapy_css)
