12a17012cade7952f286615f966fc9d18c140078
rocco8773 <eteverson@gmail.com>
2024-04-24 12:44:28 -0700

    update how the CSS and static paths are defined and set

--- plasmapy_sphinx/ext/css.py
+++ plasmapy_sphinx/ext/css.py
@@ -4,13 +4,12 @@ from plasmapy_sphinx.utils import static_dir, css_dir
 
 
 def add_plasmapy_css(app, config):
-    for path in {static_dir, css_dir}:
-        path = str(path)
-        if path not in config.html_static_path:
-            config.html_static_path.append(path)
 
-    app.add_css_file("admonition_color_contrast.css")
-    app.add_css_file("plasmapy.css", priority=501)
+    if static_dir not in config.html_static_path:
+        config.html_static_path.append(static_dir)
+
+    rel_path = css_dir.relative_to(static_dir)
+    app.add_css_file(rel_path / "plasmapy.css", priority=501)
 
 
 def setup(app: Sphinx) -> None:
--- plasmapy_sphinx/theme/__init__.py
+++ plasmapy_sphinx/theme/__init__.py
@@ -12,6 +12,7 @@ def setup(app: Sphinx) -> None:
     Sphinx ``setup()`` function for setting up the PlasmaPy theme.
     """
     css_setup(app)
+    app.setup_extension("sphinx_gallery.load_style")
 
     # Register the theme that can be referenced without adding a theme path
     app.add_html_theme("plasmapy_sphinx", theme_path=f"{theme_dir.absolute()}")
@@ -24,6 +25,3 @@ def setup(app: Sphinx) -> None:
 
     # app.setup_extension("sphinx_rtd_theme")
     sphinx_rtd_theme.setup(app)
-    app.setup_extension("sphinx_gallery.load_style")
-
-
